9adb4d731f0abfb4073e048f76b5ab1e58e84400
seansmith01 <55638721+seansmith01@users.noreply.github.com>
2023-10-31 17:04:27 +0000

    players can move and see eachother

--- Assets/Scripts/Player/PlayerNetwork.cs
+++ Assets/Scripts/Player/PlayerNetwork.cs
@@ -2,22 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.InputSystem;
 
 public class PlayerNetwork : NetworkBehaviour
 {
     [SerializeField] PlayerMovement playerMovement;
+    [SerializeField] CameraController cameraController;
+    [SerializeField] GameObject camHolder;
+    [SerializeField] Transform headTransform;
+    [SerializeField] PlayerInput playerInput;
+
+    private readonly NetworkVariable<Vector3> netPos = new NetworkVariable<Vector3>(writePerm:NetworkVariableWritePermission.Owner);
+    private readonly NetworkVariable<Quaternion> netRot = new NetworkVariable<Quaternion>(writePerm:NetworkVariableWritePermission.Owner);
+    private readonly NetworkVariable<Quaternion> headRot = new NetworkVariable<Quaternion>(writePerm:NetworkVariableWritePermission.Owner);
     // Start is called before the first frame update
     void Start()
     {
         if (IsOwner)
         {
+            playerInput.enabled = true;
             playerMovement.enabled = true;
+            cameraController.enabled = true;
+            camHolder.SetActive(true);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsOwner)
+        {
+            netPos.Value = transform.position;
+            netRot.Value = transform.rotation;
+            headRot.Value = headTransform.rotation;
+        }
+        else
+        {
+            transform.position = netPos.Value;
+            transform.rotation = netRot.Value;
+            headTransform.rotation = headRot.Value;
+        }
     }
 }
